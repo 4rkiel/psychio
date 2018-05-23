ff83b67f84b4d209899cca927d9bb689321e8587
4rk <4rkiel@gmail.com>
2018-05-23 02:50:39 +0200

    yolo

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 
 var express = require('express');
+var path = require('path');
+
 var app = express();
 
+
 app.get('/', function (req, res) {
-   res.send('Hello World');
+   res.sendFile(path.join(__dirname + '/index.html'));
 })
 
 var server = app.listen(8080, function () {
-   var host = server.address().address
-   var port = server.address().port
+   var host = server.address().address;
+   var port = server.address().port;
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Server listening at http://%s:%s", host, port);
 })
